0fd8f4fe29c2430a7d890ee461b7ea9caea708bc
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-18 12:37:52 -0400

    Update meta tags given the results from the linkin post inspector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <meta property="og:title" content="The best free workouts you can find" />
-        <meta property="og:image" content="https://drive.google.com/file/d/1EiDjejF8Rkpnakv6_G9osAFQ1KP4WZb-/view?usp=sharing" />
+        <meta property="og:title" content="Today We Lift" />
+        <meta property="og:description" content="The best free workouts you can find" />
+        <meta name="image" property="og:image" content="https://drive.google.com/file/d/1EiDjejF8Rkpnakv6_G9osAFQ1KP4WZb-/view?usp=sharing" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
