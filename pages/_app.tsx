0a7f47be7fa814b4544e33b360ca1eb2c50fcda7
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-18 12:57:21 -0400

    Update pathing for og image and extend description length

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../src/components/Layout'
-import Head from 'next/head'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../src/components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const domain = 'https://www.todaywelift.com/';
+
   return (
     <Layout>
       <Head>
         <meta property="og:title" content="Today We Lift" />
-        <meta property="og:description" content="The best free workouts you can find" />
-        <meta name="image" property="og:image" content="https://drive.google.com/file/d/1EiDjejF8Rkpnakv6_G9osAFQ1KP4WZb-/view?usp=sharing" />
+        <meta property="og:description" content="Get summer body ready with this great web app that delivers intense weight lifting workouts directly to you! Free to use and mobile friendly this is the perfect tool to streamline your gym activities." />
+        <meta name="image" property="og:image" content={`${domain}/public/weights.jpeg`} />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
     </Layout>
-
-  )
+  );
 }
+
