64bb7bbc5626d111ef0f99a7f1657f1da4f7d861
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-18 12:29:35 -0400

    Change og image to match linkedin specs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <meta property="og:title" content="The best free workouts you can find" />
-        <meta property="og:image" content="https://drive.google.com/file/d/1mzEtwc0zrmmf7IKLkHgapw4wvEMnNcDm/view?usp=sharing" />
+        <meta property="og:image" content="https://drive.google.com/file/d/1EiDjejF8Rkpnakv6_G9osAFQ1KP4WZb-/view?usp=sharing" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
